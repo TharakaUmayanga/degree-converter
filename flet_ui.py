950da2a2473dfdbd7d4c436780bfae38ccae20b1
Tharaka <tharakau@gmail.com>
2024-07-04 12:45:48 +0530

    added Degrees to DMS

--- flet_ui.py
+++ flet_ui.py
@@ -1,38 +1,52 @@
 import flet as ft
+from flet_core import theme
 
 
 def main(page: ft.Page):
     header_text = ft.Text("Degrees Converter", size=45, weight=ft.FontWeight.BOLD
                           )
-    header_row = ft.Row(
-        [
-            header_text,
 
+    page.theme = ft.Theme(color_scheme=ft.ColorScheme(primary=ft.colors.BLUE))
+    page.theme_mode = ft.ThemeMode.LIGHT
 
-        ],
-        alignment=ft.MainAxisAlignment.CENTER,
-    )
+    page.update()
+
     info_card = ft.Card(
         content=ft.Container(
             content=ft.Column(
                 [
-                    ft.ListTile(
-                        leading=ft.Icon(ft.icons.ALBUM),
-                        title=ft.Text("The Enchanted Nightingale"),
-                        subtitle=ft.Text(
-                            "Music by Julie Gable. Lyrics by Sidney Stein."
-                        ),
-                    ),
-                    ft.Row(
-                        [ft.TextButton("Buy tickets"), ft.TextButton("Listen")],
-                        alignment=ft.MainAxisAlignment.END,
-                    ),
-                ]
+                    ft.Text("Degrees to DMS"),
+                    ft.TextField(label="Enter Degrees"),
+
+                    ft.TextButton("Calculate")
+                ],
+
             ),
             width=400,
             padding=10,
-        ))
+
+        )
+    )
+    header_row = ft.Row(
+        [
+            header_text,
+
+        ],
+        alignment=ft.MainAxisAlignment.CENTER,
+    )
+
+    middle_col = ft.Column([
+        ft.Row(
+            [
+                info_card
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+        )]
+
+    )
+
     page.add(header_row)
+    page.add(middle_col)
 
 
 ft.app(main)
