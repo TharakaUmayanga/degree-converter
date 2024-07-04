b52deab19390d2e36c5008dba71af4ba0fdba552
Tharaka <tharakau@gmail.com>
2024-07-04 11:59:54 +0530

    header_row modified

--- flet_ui.py
+++ flet_ui.py
@@ -2,15 +2,37 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    header_text = ft.Text("Degrees Converter", size=45, weight=ft.FontWeight.BOLD,text_align=ft.TextAlign.CENTER,width=float("inf"),)
-    header_column = ft.Column(
-      [
-          header_text,
+    header_text = ft.Text("Degrees Converter", size=45, weight=ft.FontWeight.BOLD
+                          )
+    header_row = ft.Row(
+        [
+            header_text,
 
-      ]
+
+        ],
+        alignment=ft.MainAxisAlignment.CENTER,
     )
-    page.add(header_column)
-
+    info_card = ft.Card(
+        content=ft.Container(
+            content=ft.Column(
+                [
+                    ft.ListTile(
+                        leading=ft.Icon(ft.icons.ALBUM),
+                        title=ft.Text("The Enchanted Nightingale"),
+                        subtitle=ft.Text(
+                            "Music by Julie Gable. Lyrics by Sidney Stein."
+                        ),
+                    ),
+                    ft.Row(
+                        [ft.TextButton("Buy tickets"), ft.TextButton("Listen")],
+                        alignment=ft.MainAxisAlignment.END,
+                    ),
+                ]
+            ),
+            width=400,
+            padding=10,
+        ))
+    page.add(header_row)
 
 
 ft.app(main)
