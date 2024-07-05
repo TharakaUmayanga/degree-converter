c24e86ee06241b9db366a55283d4f627107914f7
Tharaka <tharakau@gmail.com>
2024-07-05 10:00:18 +0530

    connected function with ui

--- flet_ui.py
+++ flet_ui.py
@@ -1,6 +1,8 @@
 import flet as ft
 from flet_core import theme
 
+from main import convert_to_dms, convert_to_degrees
+
 
 def main(page: ft.Page):
     header_text = ft.Text("Degrees Converter", size=45, weight=ft.FontWeight.BOLD
@@ -13,15 +15,47 @@ def main(page: ft.Page):
 
     card_padding = 15
 
+    deg_dms_txt = ft.Text()
+    deg_dms_txt_input = ft.TextField(label="Enter Degrees")
+
+    dms_deg_txt_deg = ft.TextField(label="Enter Degrees")
+    dms_deg_txt_min = ft.TextField(label="Enter Minutes")
+    dms_deg_txt_sec = ft.TextField(label="Enter Seconds")
+
+    dms_deg_txt = ft.Text()
+
+    def calculate_deg_dms_txt_clicked(e):
+        try:
+            degrees = float(deg_dms_txt_input.value)
+            result = convert_to_dms(degrees)
+            deg_dms_txt.value = result
+            page.update()
+        except ValueError:
+            deg_dms_txt.value = "Please enter a valid number."
+            page.update()
+
+    def calculate_dms_deg_txt_clicked(e):
+        try:
+            degrees = float(dms_deg_txt_deg.value)
+            mins = float(dms_deg_txt_min.value)
+            sec = float(dms_deg_txt_sec.value)
+
+            result = convert_to_degrees(degrees,mins,sec)
+            dms_deg_txt.value = result
+            page.update()
+        except ValueError:
+            deg_dms_txt.value = "Please enter a valid number."
+            page.update()
+
     deg_dms_card = ft.Card(
         content=ft.Container(
             content=ft.Column(
                 [
                     ft.Text("Degrees to DMS"),
-                    ft.TextField(label="Enter Degrees"),
-                    ft.Text(),
+                    deg_dms_txt_input,
+                    deg_dms_txt,
 
-                    ft.FilledButton("Calculate")
+                    ft.FilledButton("Calculate", on_click=calculate_deg_dms_txt_clicked)
                 ],
 
             ),
@@ -37,11 +71,11 @@ def main(page: ft.Page):
             content=ft.Column(
                 [
                     ft.Text("DMS to Deg"),
-                    ft.TextField(label="Enter Degrees"),
-                    ft.TextField(label="Enter Minutes"),
-                    ft.TextField(label="Enter Seconds"),
-                    ft.Text(),
-                    ft.FilledButton("Calculate")
+                    dms_deg_txt_deg,
+                    dms_deg_txt_min,
+                    dms_deg_txt_sec,
+                    dms_deg_txt,
+                    ft.FilledButton("Calculate", on_click=calculate_dms_deg_txt_clicked)
                 ],
 
             ),
